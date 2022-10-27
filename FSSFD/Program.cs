3e7bbdbfe9b8c8c591e217d7bd21152a0f6ae490
Mitino94 <Mitino9494@gmail.com>
2022-10-28 00:38:50 +0300

    Добавьте файлы проекта.

---
+++ FSSFD/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace FSSFD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа через запятую");
+
+            int[] twoNumbers = Console.ReadLine().Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Console.WriteLine($"Сумма ваших волшебных чисел равна {twoNumbers[0] + twoNumbers[1]}");
+
+            Console.ReadLine();
+        }
+    }
+}
